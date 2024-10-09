2556c7559f0c78d884d1735320a6d7ec9bfd94e7
Pavel Stankevich <pavelstankevich04@gmail.com>
2024-10-09 11:20:25 +0300

    MUI Card update

--- src/StudentTable.js
+++ src/StudentTable.js
@@ -1,16 +1,23 @@
-import React from "react";
-import { Paper, Table, TableBody, TableContainer, TableHead, TableRow, TableCell, Button } from "@mui/material";
+import React, { useState } from "react";
+import { Paper, Table, TableBody, TableContainer, TableHead, TableRow, TableCell, Button, Grid2 } from "@mui/material";
+import CardInfo from "./CardInfo";
 
 const StudentTable = ({ students, delStudent }) => {
+   const [ selectedStudent, setSelectedStudent ] = useState(null);
+
+   const selectStudent = (student) => {
+      setSelectedStudent(student);
+   };
 
    return (
-      <TableContainer component={Paper}>
-         <Table sx={{ width: 400 }} aria-label="simple table">
+      <TableContainer component={Paper} sx={{backgroundColor: "#e0f2f1"}}>
+         <Table sx={{ width: 400, float: "left", marginLeft: 7 }} aria-label="simple table">
             <TableHead>
                <TableRow>
                   <TableCell align="center">Name</TableCell>
                   <TableCell align="center">Group</TableCell>
-                  <TableCell align="center">Remove</TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
                </TableRow>
             </TableHead>
             <TableBody>
@@ -21,11 +28,19 @@ const StudentTable = ({ students, delStudent }) => {
                      <TableCell align="center">
                         <Button variant="contained" onClick={() => delStudent(student.id)}>Delete</Button>
                      </TableCell>
-
+                     <TableCell>
+                        <Button variant="contained" onClick={() => selectStudent(student)} disabled={selectedStudent?.id === student.id}
+                        >Info</Button>
+                     </TableCell>
                   </TableRow>
                ))}
             </TableBody>
          </Table>
+         <Grid2 minWidth={150} maxWidth={250} sx={{ float: "right" }} marginRight={55} marginTop={10}>
+            <Grid2 container display="flex" justifyContent="center" alignItems="center">
+               {selectedStudent && <CardInfo student={selectedStudent} />}
+            </Grid2>
+         </Grid2>
       </TableContainer>
    );
 };
